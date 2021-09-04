9a84b1dc42ceaf9ce014ce6c90b3fa128b7fb880
zhanchao.wu <zhanchao.wu@icloud.com>
2021-09-04 13:19:03 +0800

    init

--- src/app.tsx
+++ src/app.tsx
@@ -6,9 +6,7 @@ import FrameworkLayout from '@/layouts/FrameworkLayout';
 const appConfig: IAppConfig = {
   app: {
     rootId: 'icestark-container',
-    addProvider: ({ children }) => (
-      <ConfigProvider prefix="next-icestark-">{children}</ConfigProvider>
-    ),
+    addProvider: ({ children }) => <ConfigProvider prefix="next-icestark-">{children}</ConfigProvider>,
   },
   router: {
     type: 'browser',
@@ -17,31 +15,36 @@ const appConfig: IAppConfig = {
     type: 'framework',
     Layout: FrameworkLayout,
     getApps: async () => {
-      const apps = [{
-        path: '/seller',
-        title: '商家平台',
-        sandbox: true,
-        // React app demo: https://github.com/alibaba-fusion/materials/tree/master/scaffolds/ice-stark-child
-        url: [
-          'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-react/build/js/index.js',
-          'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-react/build/css/index.css',
-        ],
-      }, {
-        path: '/waiter',
-        title: '小二平台',
-        sandbox: true,
-        url: [
-          // Vue app demo: https://github.com/ice-lab/vue-materials/tree/master/scaffolds/icestark-child-app
-          'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-waiter-vue/dist/js/app.js',
-          'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-waiter-vue/dist/css/app.css',
-        ],
-      }, {
-        path: '/angular',
-        title: 'Angular',
-        sandbox: true,
-        // Angular app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-common-angular
-        entry: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-common-angular/index.html',
-      }];
+      const apps = [
+        {
+          path: '/seller',
+          title: '商家平台',
+          sandbox: true,
+          // React app demo: https://github.com/alibaba-fusion/materials/tree/master/scaffolds/ice-stark-child
+          // url: [
+          //   'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-react/build/js/index.js',
+          //   'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-react/build/css/index.css',
+          // ],
+          url: ['//localhost:3334/js/index.js', '//localhost:3334/css/index.css'],
+        },
+        {
+          path: '/waiter',
+          title: '小二平台',
+          sandbox: true,
+          url: [
+            // Vue app demo: https://github.com/ice-lab/vue-materials/tree/master/scaffolds/icestark-child-app
+            'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-waiter-vue/dist/js/app.js',
+            'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-waiter-vue/dist/css/app.css',
+          ],
+        },
+        {
+          path: '/angular',
+          title: 'Angular',
+          sandbox: true,
+          // Angular app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-common-angular
+          entry: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-common-angular/index.html',
+        },
+      ];
       return apps;
     },
     appRouter: {
